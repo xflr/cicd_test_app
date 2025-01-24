488b4715ac5962dc89e6dc748549d788f5c3b759
xflr <alexandre.bressane@hotmail.com>
2025-01-24 12:55:31 -0700

    Commit 3

--- main.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("*", (req, res) => res.send("<h1>Hello World!</h1>"));
-
-app.listen(80, () => console.log("App is listening"));
